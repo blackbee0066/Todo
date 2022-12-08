2604959565507a3c60e433c4473682eaed0037bf
black bee <blessingmajoa@gmail.com>
2022-12-08 15:37:27 +0000

    added clear function to the script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+let addbtn = document.getElementById('addbtn') ;
+let clearbtn = document.getElementById('clearbtn') ;
+let mylist = document.getElementById('mylist') ;
+
+var todolistarray = [] ;
+
+var clearlist = function(){
+    todolistarray = []
+    localStorage.removeItem('mylist') ;
+    mylist.innerHTML = '' ;
+}
+
+clearbtn.addEventListener('click',clearlist) ;
