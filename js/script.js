356fe94d54106bddedd1f800ff77fa1dff4ce3f1
black bee <blessingmajoa@gmail.com>
2022-12-12 14:31:41 +0000

    buggy scripts

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 
 let addbtn = document.getElementById('addbtn') ;
+
 let clearbtn = document.getElementById('clearbtn') ;
+
 let mylist = document.getElementById('mylist') ;
+
+let inputItem = document.getElementById('inputItem');
+
 
 var todolistarray = [] ;
 
@@ -9,6 +14,30 @@ var clearlist = function(){
     todolistarray = []
     localStorage.removeItem('mylist') ;
     mylist.innerHTML = '' ;
+}
+
+
+var listItemObj = function(content, status){
+    this.content = '';
+    this.status = 'incomplete';
+}
+
+let refreshlocalstorage = function(){
+    var todo = todolistarray;
+    localStorage.removeItem('mylist');
+    localStorage.setItem ('mylist', JSON.stringify(todo)); 
+}
+
+
+
+var addlist = function(){
+    let newlistitem = new listItemObj();
+    newlistitem.content = inputItem.value;
+    todolistarray.push(newlistitem);
+    //Refresh
+    refreshlocalstorage();
+    var newitem = createItemDom(inputItem.value, 'incomplete');
+    mylistitem.value = '';
 }
 
 clearbtn.addEventListener('click',clearlist) ;
